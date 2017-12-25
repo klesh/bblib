77cd9eca9b7c899631253ad08d51d4a6ad4fd540
Klesh Wong <klesh@qq.com>
2017-12-25 10:07:12 +0800

    [bugfix] remove hard dependencies

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 'use strict';
 
+/*
 exports.fs = require('./fs');
-exports.ssh2 = require('./ssh');
+exports.ssh2 = require('./ssh2');
 exports.sh = require('./sh');
 exports.prompt = require('./prompt');
 exports.request = require('./request');
 exports.RestClient = require('./rest-client');
+*/
